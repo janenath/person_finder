cc68c1b3a662f88e56568e419bc6fa9808d0fa86
Jane <jane.e.nath@gmail.com>
2020-07-29 09:14:55 -0700

    create and style header text

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <main>
+        <div className="instructions">
+          <h1>The Person Finder</h1>
+          <p className="instructionText">If you just can't find someone and need to know what they look like, you've come to the right place! Just type the name of the person you are looking for below into the search box!</p>
+        </div>
+      </main>
     </div>
   );
 }
