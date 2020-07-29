ff4c991d266c1cd9d7ff2e3d8454c5e0fb58ecb2
Jane <jane.e.nath@gmail.com>
2020-07-29 10:29:26 -0700

    initialize state and set state with query

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactLoading from 'react-loading';
 
 import './App.scss';
 
 import Header from './components/Header';
 import Card from './components/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <main>
-        <div className="instructions">
-          <h1>The Person Finder</h1>
-          <p className="instructionText">If you just can't find someone and need to know what they look like, you've come to the right place! Just type the name of the person you are looking for below into the search box!</p>
-        </div>
-        <div className="search">
-          <input type="text" placeholder="Type a name..."></input>
-          <Card/>
-          <Card/>
-          <Card/>
-        </div>
-      </main>
-    </div>
-  );
+class App extends Component {
+  state = {
+    loading: false,
+    query: '',
+    results: [],
+    errorMessage: ''
+  }
+
+  handleInputChange = (event) => {
+    const query = event.target.value;
+    this.setState({query: query, loading: true, errorMessage: ''})
+  }
+
+  getSearchResults = (query) => {
+
+  }
+
+  render() {
+    const { query } = this.state;
+    return (
+      <div className="App">
+        <Header/>
+        <main>
+          <div className="instructions">
+            <h1>The Person Finder</h1>
+            <p className="instructionText">If you just can't find someone and need to know what they look like, you've come to the right place! Just type the name of the person you are looking for below into the search box!</p>
+          </div>
+          <div className="search">
+            <label className="search" htmlFor="search-input">
+              <input 
+                type="text" 
+                placeholder="Type a name..." 
+                id="search-input" 
+                value={query}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <Card/>
+            <Card/>
+            <Card/>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
